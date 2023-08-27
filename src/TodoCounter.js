0c8d97de60622121c19e92fbceb9d4ecd6133adf
Angel Sulbaran <angeldv.sulbaran@gmail.com>
2023-08-27 14:46:16 +0200

    icons added

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,9 +2,18 @@ import React from "react";
 import './TodoCounter.css';
 
 function TodoCounter ({total , completed}){
+  if(completed===total){
+    return <h1 style={{ display: 'flex',
+      alignItems:"center",
+
+    
+    }} > felicitaciones completaste todos los todo's</h1>
+  }
+
     return(
       <h1 className="TodoCounter">
         Has Completado {completed} de {total} Todo's
+        
       </h1>
     )
   }
